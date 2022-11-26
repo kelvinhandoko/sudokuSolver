dadffc48b310690e18ea99d970b6077a29b16b26
kelvinhandoko <kelvinhandoko03@gmail.com>
2022-11-26 18:38:26 +0700

    feat: done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const board = document.getElementById("board")
 const button = document.getElementById("answerButton")
+const resetButton = document.getElementById("resetButton")
 const grid = []
 const grid0 = []
 const grid1 = []
@@ -10,6 +11,47 @@ const grid5 = []
 const grid6 = []
 const grid7 = []
 const grid8 = []
+const possible = (board, y, x, n) => {
+    // check row n col
+    for (let i = 0; i < 9; i++) {
+        if (board[y][i] == n || board[i][x] == n) {
+            return false
+        }
+    }
+
+    const col0 = Math.floor(x / 3) * 3
+    const row0 = Math.floor(y / 3) * 3
+    // check square
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            if (board[row0 + row][col0 + col] == n) {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+function solve(board) {
+    for (let y = 0; y < 9; y++) {
+        for (let x = 0; x < 9; x++) {
+            if (board[y][x] === 0) {
+                for (let num = 1; num <= 9; num++) {
+                    if (possible(board, y, x, num)) {
+                        board[y][x] = num
+
+                        if (solve(board)) return board
+                    }
+                }
+
+                board[y][x] = 0
+                return false
+            }
+        }
+    }
+
+    return board
+}
 
 for (let i = 0; i < 81; i++) {
     inputElement = document.createElement("input")
@@ -45,10 +87,16 @@ button.addEventListener("click", () => {
         }
     })
     grid.push(grid0, grid1, grid2, grid3, grid4, grid5, grid6, grid7, grid8)
+    solve(grid)
+    flattedGrid = grid.flat(8)
+
+    allInput.forEach((input, index) => {
+        input.value = flattedGrid[index]
+    })
 })
-const possible = (y, x, number) => {
-    for (let i = 0; i < 9; i++) {
-        if (grid[y][i])
-        
-    }
-}
+
+resetButton.addEventListener("click", () => {
+    allInput.forEach(input => {
+        input.value = ""
+    })
+})
